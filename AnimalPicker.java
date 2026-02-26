07d2997cc34cea32f0901658c3e51387dc94563c
nharding925 <nharding925@gmail.com>
2026-02-25 20:50:48 -0500

    Added user input and output

---
+++ AnimalPicker.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AnimalPicker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //user input
+        System.out.println("Which animal would you like to see? (dog/cat)");
+        String choice = scanner.nextLine().toLowerCase();
+
+        //print cat or dog
+        if (choice.equals("dog")) {
+            System.out.println("DOG ASCII ART HERE");
+        } else if (choice.equals("cat")) {
+            System.out.println("CAT ASCII ART HERE");
+        }
+    }
+}
