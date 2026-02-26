4d0deaef8445e8d8445615b650e5d0a24aff67f3
nharding925 <nharding925@gmail.com>
2026-02-25 21:04:44 -0500

    add loop to prompt

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -3,19 +3,24 @@ import java.util.Scanner;
 public class AnimalPicker {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String choice = "";
 
-        //user input
-        System.out.println("Which animal would you like to see? (dog/cat)");
-        String choice = scanner.nextLine().toLowerCase();
+        //keep asking until valid input
+        while (!choice.equals("dog") && !choice.equals("cat")) {
+            //user input
+            System.out.println("Which animal would you like to see? (dog/cat)");
+            choice = scanner.nextLine().toLowerCase();
 
+            //error message
+            if (!choice.equals("dog") && !choice.equals("cat")) {
+                System.out.println("Error: Invalid option. Please enter 'dog' or 'cat'.");
+            }
+        }
         //print cat or dog
         if (choice.equals("dog")) {
             System.out.println("DOG ASCII ART HERE");
         } else if (choice.equals("cat")) {
             System.out.println("CAT ASCII ART HERE");
-        } else {
-            //error message
-            System.out.println("Error: Invalid option. Please enter 'dog' or 'cat'.");
         }
     }
 }
